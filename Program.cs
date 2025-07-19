f5aaed764d2788881c676b13b652710a9ead410e
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2025-07-19 15:05:20 +0300

    Add DBContext

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using AwesomeCompany;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DatabaseContext>(
+    e => e.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+var app = builder.Build();
+app.UseHttpsRedirection();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
